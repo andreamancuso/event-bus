4c73d25add5bcdc858e34db2f2b59c693a46396c
Andrea <and.mancuso@googlemail.com>
2018-01-31 20:37:16 +0100

    Fixed indentation.

--- test/event-bus.test.js
+++ test/event-bus.test.js
@@ -1,34 +1,35 @@
 import chai from 'chai';
+
 var expect = chai.expect;
 
 import EventBus from '../src/event-bus';
 
 describe('EventBus', () => {
-	let eventBus;
+    let eventBus;
 
-	beforeEach(() => {
-		eventBus = new EventBus();
-	});
+    beforeEach(() => {
+        eventBus = new EventBus();
+    });
 
-	it('should call one handler when an action is fired', (done) => {
+    it('should call one handler when an action is fired', (done) => {
         eventBus.on('one', () => {
             done();
         });
         eventBus.emit('one');
 
-	});
+    });
 
-	it('supports Symbols', (done) => {
-	    const one = Symbol('one');
+    it('supports Symbols', (done) => {
+        const one = Symbol('one');
         eventBus.on(one, () => {
             done();
         });
         eventBus.emit(one);
 
-	});
+    });
 
-	it('should call multiple handler when an action is fired', () => {
-	    let counter = 0;
+    it('should call multiple handler when an action is fired', () => {
+        let counter = 0;
         eventBus.on('two', () => {
             counter++;
         });
@@ -40,19 +41,19 @@ describe('EventBus', () => {
         eventBus.emit('two');
 
         expect(counter).to.equal(4);
-	});
+    });
 
-	it('should call action handler(s) with one parameter', () => {
+    it('should call action handler(s) with one parameter', () => {
         eventBus.on('echo', (str) => {
             expect(str).to.equal('hello world');
         });
 
         eventBus.emit('echo', 'hello world');
 
-	});
+    });
 
-	it('should call the action handler with multiple parameters', () => {
-        eventBus.on('log', function() {
+    it('should call the action handler with multiple parameters', () => {
+        eventBus.on('log', function () {
             expect(arguments.length).to.equal(3);
             expect(arguments[0]).to.equal('hello');
             expect(arguments[1]).to.equal('world');
@@ -60,13 +61,13 @@ describe('EventBus', () => {
         });
 
         eventBus.emit('log', 'hello', 'world', '!');
-	});
+    });
 
-	it('should allow to specify a custom context object', () => {
-	    const obj = {
-	        counter: 0,
-	        method() {
-	            this.counter++;
+    it('should allow to specify a custom context object', () => {
+        const obj = {
+            counter: 0,
+            method() {
+                this.counter++;
             }
         };
 
@@ -76,12 +77,14 @@ describe('EventBus', () => {
         eventBus.emit('log');
 
         expect(obj.counter).to.equal(2);
-	});
+    });
 
-	it('should allow to unsubscribe from an event', () => {
-	    let counter = 0;
+    it('should allow to unsubscribe from an event', () => {
+        let counter = 0;
 
-        const handler = () => {counter++};
+        const handler = () => {
+            counter++
+        };
 
         eventBus.on('log', handler);
 
@@ -92,9 +95,9 @@ describe('EventBus', () => {
         eventBus.emit('log');
 
         expect(counter).to.equal(1);
-	});
+    });
 
-	it('should allow to unsubscribe from an event, specifying a context object', () => {
+    it('should allow to unsubscribe from an event, specifying a context object', () => {
         const obj = {
             counter: 0,
             method() {
@@ -111,9 +114,9 @@ describe('EventBus', () => {
         eventBus.emit('log');
 
         expect(obj.counter).to.equal(1);
-	});
+    });
 
-	it('should prevent action handlers to be subscribed to a specified event more than once', () => {
+    it('should prevent action handlers to be subscribed to a specified event more than once', () => {
         const obj = {
             counter: 0,
             method() {
@@ -128,5 +131,5 @@ describe('EventBus', () => {
         eventBus.emit('log');
 
         expect(obj.counter).to.equal(2);
-	});
+    });
 });
